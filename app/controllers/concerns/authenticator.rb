060acf4156ebf632b8b22cde0e5dcb0e03f005a6
Ashutosh Raj <anshu2212@gmail.com>
2020-05-10 03:06:17 +0530

    path changed

--- app/controllers/concerns/authenticator.rb
+++ app/controllers/concerns/authenticator.rb
@@ -47,7 +47,7 @@ module Authenticator
       url = if cookies[:return_to] && !dont_redirect_to.include?(cookies[:return_to])
         cookies[:return_to]
       else
-        root_url + relative_root + '/u/home'
+        relative_root + '/u/home'
       end
 
       # Delete the cookie if it exists
